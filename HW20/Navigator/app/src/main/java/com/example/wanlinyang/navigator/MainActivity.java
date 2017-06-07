b0bb79993278c51f7f0d88e89894b59010981a38
WanlinYang <wanlinyang2018@u.northwestern.edu>
2017-06-07 15:50:29 -0500

    Navigator

--- HW20/Navigator/app/src/main/java/com/example/wanlinyang/navigator/MainActivity.java
+++ HW20/Navigator/app/src/main/java/com/example/wanlinyang/navigator/MainActivity.java
@@ -29,9 +29,37 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
-public class MainActivity extends AppCompatActivity {
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-    SeekBar myControl;
+import java.io.IOException;
+
+import static android.graphics.Color.blue;
+import static android.graphics.Color.green;
+import static android.graphics.Color.red;
+import static android.graphics.Color.rgb;
+
+
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener{
+
+    SeekBar myControl1;
+    SeekBar myControl2;
     TextView myTextView;
     Button button;
     TextView myTextView2;
@@ -43,17 +71,34 @@ public class MainActivity extends AppCompatActivity {
     private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
     private SerialInputOutputManager mSerialIoManager;
 
+    private Camera mCamera;
+    private TextureView mTextureView;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
+    private Bitmap bmp = Bitmap.createBitmap(640, 480, Bitmap.Config.ARGB_8888);
+    private Canvas canvas = new Canvas(bmp);
+    private Paint paint1 = new Paint();
+    private TextView mTextView;
+
+    int thresh;
+    int COM;
+    int startY;
+    static long prevtime = 0; // for FPS calculation
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // keeps the screen from turning off
 
-        myControl = (SeekBar) findViewById(R.id.seek1);
+        myControl1 = (SeekBar) findViewById(R.id.seek1);
+        myControl2 = (SeekBar) findViewById(R.id.seek2);
 
         myTextView = (TextView) findViewById(R.id.textView01);
         myTextView.setText("Enter whatever you Like!");
 
-        setMyControlListener();
+        setMyControlListener1();
+        setMyControlListener2();
 
         myTextView2 = (TextView) findViewById(R.id.textView02);
         myScrollView = (ScrollView) findViewById(R.id.ScrollView01);
@@ -63,8 +108,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myTextView2.setText("value on click is "+myControl.getProgress());
-                String sendString = String.valueOf(myControl.getProgress()) + '\n';
+                myTextView2.setText("value on click is "+myControl1.getProgress());
+                String sendString = String.valueOf(myControl1.getProgress()) + '\n';
                 try {
                     sPort.write(sendString.getBytes(), 10); // 10 is the timeout
                 } catch (IOException e) { }
@@ -74,11 +119,33 @@ public class MainActivity extends AppCompatActivity {
 
         manager = (UsbManager) getSystemService(Context.USB_SERVICE);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+            mSurfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+            mSurfaceHolder = mSurfaceView.getHolder();
+
+            mTextureView = (TextureView) findViewById(R.id.textureview);
+            mTextureView.setSurfaceTextureListener(this);
+
+            mTextView = (TextView) findViewById(R.id.cameraStatus);
+            myControl2 = (SeekBar) findViewById(R.id.seek2);
+
+            // set the paintbrush for writing text on the image
+            paint1.setColor(0xffff0000); // red
+            paint1.setTextSize(24);
+
+            setMyControlListener2();
+
+            mTextView.setText("started camera");
+        } else {
+            mTextView.setText("no camera permissions");
+        }
+
 
     }
 
-    private void setMyControlListener() {
-        myControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+    private void setMyControlListener1() {
+        myControl1.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 
             int progressChanged = 0;
 
@@ -86,6 +153,30 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChanged = progress;
                 myTextView.setText("The value is: "+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
+    }
+
+    private void setMyControlListener2() {
+        myControl2.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            int progressChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+                thresh = progress;
+                //myTextView.setText("The value is: "+progress);
             }
 
             @Override
@@ -208,6 +299,97 @@ public class MainActivity extends AppCompatActivity {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+    }
+
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        mCamera = Camera.open();
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPreviewSize(640, 480);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY); // no autofocusing
+        parameters.setAutoExposureLock(true); // keep the white balance constant
+        mCamera.setParameters(parameters);
+        mCamera.setDisplayOrientation(90); // rotate to portrait mode
+
+        try {
+            mCamera.setPreviewTexture(surface);
+            mCamera.startPreview();
+        } catch (IOException ioe) {
+            // Something bad happened
+        }
+    }
+
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        // Ignored, Camera does all the work for us
+    }
+
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        mCamera.stopPreview();
+        mCamera.release();
+        return true;
+    }
+
+    public int abs(int n){
+        if (n < 0){
+            int m = -n;
+            return -m;
+        } else {
+            return n;
+        }
+    }
+
+    // the important function
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        // every time there is a new Camera preview frame
+        mTextureView.getBitmap(bmp);
+
+        final Canvas c = mSurfaceHolder.lockCanvas();
+        if (c != null) {
+            int[] pixels = new int[bmp.getWidth()]; // pixels[][] is the RGBA data
+            startY = bmp.getHeight()*5/10; // which row in the bitmap to analyze to read
+            bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
+            COM = 0;  // center
+
+            int sum_mr = 0;  // the sum of the mass times the radius
+            int sum_m = 0;  // the sum of the masses
+
+            for (int i = 0; i < bmp.getWidth(); i++){
+                int gsr = abs(green(pixels[i])-red(pixels[i]));
+                int gsb = abs(green(pixels[i])-blue(pixels[i]));
+                int rsb = abs(red(pixels[i])-blue(pixels[i]));
+
+                if ( gsr<thresh && gsb<thresh && rsb<thresh && green(pixels[i])>(thresh*2) ){   // gray, and brightness
+                    pixels[i] = rgb(255, 0, 0);
+
+                    sum_m = sum_m + green(pixels[i])+red(pixels[i])+blue(pixels[i]);
+                    sum_mr = sum_mr + (green(pixels[i])+red(pixels[i])+blue(pixels[i]))*i;
+                }
+            }
+            bmp.setPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
+
+            // only use the data if there were a few pixels identified, otherwise you might get a divide by 0 error
+            if(sum_m>5){
+                COM = sum_mr / sum_m;
+            }
+            else{
+                COM = 0;
+            }
+        }
+
+        // draw a circle at some position
+        if(COM != 0){
+            canvas.drawCircle(COM, startY , 5, paint1); // x position, y position, diameter, color
+        }
+
+        // write the pos as text
+        canvas.drawText("Thresh = " + thresh, 10, 200, paint1);
+        c.drawBitmap(bmp, 0, 0, null);
+        mSurfaceHolder.unlockCanvasAndPost(c);
+
+        // calculate the FPS to see how fast the code is running
+        long nowtime = System.currentTimeMillis();
+        long diff = nowtime - prevtime;
+        mTextView.setText("FPS " + 1000 / diff);
+        prevtime = nowtime;
     }
 
 }
